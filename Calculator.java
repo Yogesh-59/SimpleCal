7ab4d2ceec4fc70352b433b80241686fb3cb4335
Deepraj Lodhi <deeprajlodhi59@gmail.com>
2025-01-07 10:34:28 +0530

    Addition Methode added

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,13 @@
 import java.util.*;
 class Calculator{
-    
+    public static int Addition(int num1, int num2){
+        return num1+num2;
+    }
     public static void main(String[] args) {
-        System.out.println("This is Calculator");
-       
+        System.out.println("This is Calculator"); 
+        Scanner sc=new Scanner(System.in);
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+            System.out.println("Addition"+Addition(num1, num2));
     }
 }
