3963f6e6c4b62d5d3ba361e916dadecfcca3f283
Deepraj Lodhi <deeprajlodhi59@gmail.com>
2025-01-07 11:04:53 +0530

    Sustraction Method Added

--- Calculator.java
+++ Calculator.java
@@ -1,13 +1,13 @@
 import java.util.*;
 class Calculator{
-    public static int Addition(int num1, int num2){
-        return num1+num2;
+    public static int Subtraction(int num1, int num2){
+        return num1-num2;
     }
     public static void main(String[] args) {
         System.out.println("This is Calculator"); 
         Scanner sc=new Scanner(System.in);
         int num1=sc.nextInt();
         int num2=sc.nextInt();
-            System.out.println("Addition"+Addition(num1, num2));
+            System.out.println("Subtraction Answer is"+Subtraction(num1, num2 ));
     }
 }
