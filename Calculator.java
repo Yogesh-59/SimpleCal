27d6bcd350082b898df7ce32d2bc231baec23f27
Deepraj Lodhi <deeprajlodhi59@gmail.com>
2025-01-07 11:14:54 +0530

    Multiplication

--- Calculator.java
+++ Calculator.java
@@ -1,13 +1,13 @@
 import java.util.*;
 class Calculator{
-    public static int Subtraction(int num1, int num2){
-        return num1-num2;
+    public static int Multiplication(int num1, int num2){
+        return num1*num2;
     }
     public static void main(String[] args) {
         System.out.println("This is Calculator"); 
         Scanner sc=new Scanner(System.in);
         int num1=sc.nextInt();
         int num2=sc.nextInt();
-            System.out.println("Subtraction Answer is"+Subtraction(num1, num2 ));
+            System.out.println("Multiplication Answer is"+Multiplication(num1, num2 ));
     }
 }
